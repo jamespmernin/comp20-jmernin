dfd68db0ea2785ddb23936b5b532ab1339f635aa
jmernin1992 <jmernin1992@gmail.com>
2015-10-09 20:37:52 -0400

    First Lab 5 submission

---
+++ duckhunt/README.md
@@ -0,0 +1,14 @@
+James Mernin
+Chow
+Web Programming
+10/9/2015
+
+Lab 5
+
+Correctly Implemented: Everything but the bonus animation
+
+Incorrectly Implemented: Nothing
+
+Collaboration: No one
+
+Hours Spent: 1
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,23 @@
-// Replace this with your code...
+function init() {
+	var canvas = document.getElementById('game_canvas');
+	var ctx = canvas.getContext('2d');
+
+	var img = new Image();
+
+	img.onload = function() {
+		ctx.drawImage(img, 0, 0);
+	};
+	img.src = 'duckhunt-background.gif';
+
+	var duck1 = new Image();
+	duck1.onload = function() {
+		ctx.drawImage(duck1, 0, 115, 34, 34, 50, 50, 34, 34);
+	};
+	duck1.src = 'duckhunt_various_sheet.png';
+
+	var duck2 = new Image();
+	duck2.onload = function() {
+		ctx.drawImage(duck2, 206, 115, 42, 34, 175, 25, 34, 34);
+	}
+	duck2.src = 'duckhunt_various_sheet.png';
+};
